79c83f73afbfa6d558a45c08008095579858b6d1
Ken Sze <ken.yksze@gmail.com>
2025-03-17 12:36:21 -0400

    initial commits

---
+++ src/components/TabBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TabBar = ({ tabs, currentTab }) => {
+  return (
+    <div id="tab-bar">
+      {tabs.map((tab) => (
+        <img></img>
+      ))}
+    </div>
+  );
+};
+
+export default TabBar;
